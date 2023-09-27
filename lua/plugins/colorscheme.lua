a64f8f82008d3d00068627aa49d6cafe3d468a24
apesteguia <mikeltxomikeltxo@gmail.com>
2023-09-27 21:17:38 +0200

    first commitç

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+--
+-- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+--
+-- File: plugins/colorscheme.lua
+-- Description: Colorscheme config
+-- Author: Kien Nguyen-Tuan <kiennt2609@gmail.com>
+return { {
+  -- Rose-pine - Soho vibes for Neovim
+  "rose-pine/neovim",
+  name = "rose-pine",
+  opts = {
+    dark_variant = "main",
+    disable_background = true
+  }
+} }
